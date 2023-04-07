77e081d80d66a33dc376fc0014ad4883f91ae996
cgathua <114153640+cgathua@users.noreply.github.com>
2023-04-07 13:58:09 -0400

    edit: files

--- index.js
+++ index.js
@@ -11,3 +11,47 @@ faqs.forEach((faq) => {
     });
 });
 // END OF FAQ JAVASCRIPT
+
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+const menu = document.getElementById('navbar2');
+//end of open & close nav menu
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+   
+}); 
+ //end of open sidebar
+
+ closeBtn.addEventListener('click', () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display ='inline-block';
+}); //end of close sidebar
+// END OF HAMBURGER MENU
+
+const navItems = document.querySelectorAll('nav ul li');
+
+const removeActiveClass = () => {
+    navItems.forEach(item => {
+        const link = item.querySelector('a');
+        link.classList.remove('active');
+    })
+} // remove active class from other items
+
+navItems.forEach(item => {
+    const link  = item.querySelector('a');
+    link.addEventListener('click', () =>{
+        removeActiveClass();
+        link.classList.add('active');   
+    })   
+}) //add active class to clicked nav item
+// END OF DESKTOP ACTIVE CLASS LINKS
+
+
+
+
+
+ 
